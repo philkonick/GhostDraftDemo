f65d8c74eb0608d8db24222f17af6c0e7b56cee6
Philip Konick <philipk@bumblebee.co.za>
2023-10-04 11:16:31 +0200

    Styling tweaks

--- src/app/components/view-document-for-review/view-document-for-review.component.ts
+++ src/app/components/view-document-for-review/view-document-for-review.component.ts
@@ -1,18 +1,4 @@
 import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-
-export interface Reviewers {
-  name: string;
-  status: string;
-  comment: string;
-}
-const ELEMENT_DATA: Reviewers[] = [
-  { name: 'JaneD', status: 'Approved', comment: 'Looks good to me' },
-  { name: 'FredF', status: 'Rejected', comment: 'Formatting is inconsistent' },
-  { name: 'MaryC', status: 'Not Reviewed', comment: '' },
-];
-
-// Above is demo data, normally data would be imported via the dialog, either in entirety or my passing an ID which is then used in a backend call for additional information
 
 @Component({
   selector: 'app-view-document-for-review',
@@ -20,6 +6,14 @@ const ELEMENT_DATA: Reviewers[] = [
   styleUrls: ['./view-document-for-review.component.scss'],
 })
 export class ViewDocumentForReviewComponent {
-  displayedColumns: string[] = ['name', 'status', 'comment'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  // below is demo data, normally data would be imported via the dialog, either in entirety or my passing an ID which is then used in a backend call for additional information
+  reviewers = [
+    { name: 'JaneD', status: 'Approved', comment: 'Looks good to me' },
+    {
+      name: 'FredF',
+      status: 'Rejected',
+      comment: 'Formatting is inconsistent',
+    },
+    { name: 'MaryC', status: 'Not Reviewed', comment: '' },
+  ];
 }
